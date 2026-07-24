730748582153e9e27f00368c0f5dab4424d6f40e
Yash Vardhan <ranjukrish1975@gmail.com>
2026-07-24 09:53:13 +0530

    Update strategy.cpp

--- proj/src/engine/strategy.cpp
+++ proj/src/engine/strategy.cpp
@@ -1,6 +1,6 @@
 #include "strategy.hpp"
 #include <iostream>
-#include <emmintrin.h> // For _mm_pause()
+#include <emmintrin.h> 
 
 void StrategyEngine::run(SPSCQueue<Order>& inbound_queue) {
     std::cout << "[Strategy] Engine thread started. Spinning for orders...\n";
@@ -10,16 +10,14 @@ void StrategyEngine::run(SPSCQueue<Order>& inbound_queue) {
 
     while (true) {
         if (inbound_queue.pop(incoming_order)) {
-            // In a real system, you calculate alpha, check risk, and fire an outbound order here.
+            
             executed_count++;
-
-            // We only print every 100,000 orders so we don't slow down the CPU with I/O bottlenecks.
+            
             if (executed_count % 100000 == 0) {
                 std::cout << "[Strategy] Processed " << executed_count 
                           << " orders (Last Order ID: " << incoming_order.orderId << ")\n";
             }
         } else {
-            // Hardware Hint: Queue is empty, yield CPU resources briefly
             _mm_pause();
         }
     }
